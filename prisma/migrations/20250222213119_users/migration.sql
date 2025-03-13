3b4f8b4e24fa84eee3cd43f53e9bdb20044e6f2c
Willian Souza <52672785+wrsouza@users.noreply.github.com>
2025-03-12 23:54:50 -0300

    feat: add database schema

--- prisma/migrations/20250222213119_users/migration.sql
+++ prisma/migrations/20250222213119_users/migration.sql
@@ -6,8 +6,8 @@ CREATE TABLE "users" (
     "password" VARCHAR(255) NOT NULL,
     "is_admin" BOOLEAN NOT NULL,
     "is_active" BOOLEAN NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
--- prisma/migrations/20250222213200_roles/migration.sql
+++ prisma/migrations/20250222213200_roles/migration.sql
@@ -3,8 +3,8 @@ CREATE TABLE "roles" (
     "id" UUID NOT NULL,
     "name" VARCHAR(50) NOT NULL,
     "description" VARCHAR(255) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "roles_pkey" PRIMARY KEY ("id")
 );
